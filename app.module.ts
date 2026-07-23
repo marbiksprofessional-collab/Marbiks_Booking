61d6b90c399f8aedac5132ea1d76b92288078591
marbiksprofessional-collab <marbiksprofessional@gmail.com>
2026-07-24 00:50:28 +0530

    fixed and connected smart booking engine[2.3,2.5]

--- app.module.ts
+++ app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ErpService } from './erp-service';
+import { BookingEngine } from './booking-engine';
 
 @Module({
-  imports: [], // Future microservices like BookingModule, ErpSyncModule will go here
-    controllers: [AppController], // Handles incoming requests from the app
-      providers: [AppService], // Handles the business logic and database communication
-      })
-      export class AppModule {}
-      
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, ErpService, BookingEngine],
+})
+export class AppModule {}
